0501b0d3f7b59937f249b22b0fd214c2567982c1
Moamen Sherif Abdelkader <moamensherif19@gmail.com>
2025-02-25 07:20:52 +0200

    last

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -17,18 +17,6 @@ class PurchaseOrder(models.Model):
     exceed_discount_limit = fields.Boolean()
 
     def button_confirm(self):
-        for order in self:
-            if order.state not in ['draft', 'sent', 'waiting']:
-                continue
-            order.order_line._validate_analytic_distribution()
-            order._add_supplier_to_product()
-            # Deal with double validation process
-            if order._approval_allowed():
-                order.button_approve()
-            else:
-                order.write({'state': 'to approve'})
-            if order.partner_id not in order.message_partner_ids:
-                order.message_subscribe([order.partner_id.id])
 
         res = super(PurchaseOrder, self).button_confirm()
         for rec in self:
@@ -39,14 +27,3 @@ class PurchaseOrder(models.Model):
             if rec.exceed_discount_limit:
                 rec.state = 'waiting'
         return res
-
-    # def button_confirm(self):
-    #     res = super(PurchaseOrder, self).button_confirm()
-    #     for rec in self:
-    #         if self.env.user.has_group('purchase.group_purchase_manager'):
-    #             if rec.state == 'waiting':
-    #                 rec.state = 'purchase'
-    #             continue
-    #         if rec.exceed_discount_limit:
-    #             rec.state = 'waiting'
-    #     return res
